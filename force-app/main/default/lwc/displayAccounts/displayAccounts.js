250252d9722351ae95e81433af9c3c7b10a4b1ed
elyas-kosari <elyas.kosari@gmail.com>
2023-08-10 11:40:28 -0400

    Transform Data using Map function

--- force-app/main/default/lwc/displayAccounts/displayAccounts.js
+++ force-app/main/default/lwc/displayAccounts/displayAccounts.js
@@ -5,9 +5,28 @@ export default class DisplayAccounts extends LightningElement {
 
   disabled = false;
   annualRevenue = 30000000;
+  accountList = [];
+  error;
+
+  // @wire(getAccounts, {annualRevenue: "$annualRevenue"})
+  // accountList; // accountList.data, accountList.error
 
   @wire(getAccounts, {annualRevenue: "$annualRevenue"})
-  accountList;
+  wired_accountList({data, error}) { // wired_accountList(result) --> result.data, result.error
+    console.log(data);
+    this.accountList = [];
+    if (data) { // if (result.data)
+      this.accountList = data.map(element => ({
+        accountId: element.Id,
+        label: element.Name,
+        value: element.AnnualRevenue
+    })); // this.accountList = result.data;
+      this.error = undefined;
+    } else if (error) { // else if (result.error)
+      this.accountList = undefined;
+      this.error = error;  // this.error = result.error
+    }
+  }
 
   handleAccounts() {
     this.disabled = !this.disabled;
