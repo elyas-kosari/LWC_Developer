f1c94f10e94e64644463fae3e1b34bf10f6742c0
elyas-kosari <elyas.kosari@gmail.com>
2023-08-10 10:54:32 -0400

    Adding Toggle Button to Wire Service

--- force-app/main/default/lwc/displayAccounts/displayAccounts.js
+++ force-app/main/default/lwc/displayAccounts/displayAccounts.js
@@ -3,10 +3,12 @@ import getAccounts from "@salesforce/apex/AccountData.getAccounts";
 
 export default class DisplayAccounts extends LightningElement {
 
+  disabled = false;
+
   @wire(getAccounts)
   accountList;
 
-  handleAccounts(event) {
-    console.log(JSON.parse(JSON.stringify(this.accountList.data)));
+  handleAccounts() {
+    this.disabled = !this.disabled;
   }
 }
