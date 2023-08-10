ea39c74d84b1fb668cce42fda8683f280f7a1d4e
elyas-kosari <elyas.kosari@gmail.com>
2023-08-10 09:59:38 -0400

    HTML Directives

---
+++ force-app/main/default/lwc/htmlDirectives/htmlDirectives.js
@@ -0,0 +1,16 @@
+import { LightningElement } from 'lwc';
+
+export default class HtmlDirectives extends LightningElement {
+  accountId = "0016s00000dNva3AAC";
+
+  studentList = [
+    {studentId: 1, studentName:"MeAnna"},
+    {studentId: 2, studentName:"Vin"},
+    {studentId: 3, studentName:"Ariana"},
+    {studentId: 4, studentName:"Karen"},
+    {studentId: 5, studentName:"Elise"},
+    {studentId: 6, studentName:"Amanda"},
+    {studentId: 7, studentName:"Mercedes"},
+    {studentId: 8, studentName:"Elyas"},
+  ];
+}
